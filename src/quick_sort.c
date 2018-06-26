c545dec5d8ed754c4859c5805d3875a860765d6d
Kazuya-Kawahara <kawahara.k.ad@m.titech.ac.jp>
2018-06-26 16:39:49 +0900

    課題1終わり

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -9,6 +9,28 @@ int A[N];
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+    if(n <= 1){
+        //配列の要素数が0or1の場合何も行わない
+    }
+    else{
+        int i, j, pivot;
+        pivot = A[0];  //先頭要素をピボットとする
+        for(i = j = 1; i < n; i++){
+            if(A[i] <= pivot){
+                int z = A[j];
+                A[j] = A[i];
+                A[i] = z;
+                j++;
+            }
+        }
+        int z = A[j-1];
+        A[j-1] = A[0];
+        A[0] = z;  //ピボットをピボット以下を寄せ集めた配列の最後尾に移動する
+        
+        quick_sort(A, j-1);  //ピボット以下を寄せ集めた配列の最後尾(j-1番目)以前の配列(要素数j-2+1)に同様の操作を行う
+        quick_sort(A+j, n-j);  //残りについても同様
+    }
+    
 }
 
 int main(){
